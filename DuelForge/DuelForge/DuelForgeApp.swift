def1d1c3cc06b2dec05d995e3b801e4aef24229f
devjeancarlos <devjeancarlos@gmail.com>
2026-07-25 00:11:14 -0500

    Initial project setup

---
+++ DuelForge/DuelForge/DuelForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuelForgeApp.swift
+//  DuelForge
+//
+//  Created by Jean Carlos Ramos Cruz on 24/07/26.
+//
+
+import SwiftUI
+
+@main
+struct DuelForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DuelForge/DuelForgeTests/DuelForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DuelForgeTests.swift
+//  DuelForgeTests
+//
+//  Created by Jean Carlos Ramos Cruz on 24/07/26.
+//
+
+import Testing
+@testable import DuelForge
+
+struct DuelForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
